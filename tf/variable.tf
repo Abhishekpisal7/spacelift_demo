840547bc9b3aebfa87acc0d5e59ef2c50677f8cd
AbhishekPisal <Abhishekshivajipisal@gmail.com>
2025-08-22 01:08:52 +0530

    file is modified

--- tf/variable.tf
+++ tf/variable.tf
@@ -2,6 +2,6 @@ variable "public_key" {
     
     description = "Key Value for instance"
     type = string
-    default = "/mnt/workspace/agent.pub"
+    default = "/mnt/workspace/my_rsa_key.pub"
 
 }
